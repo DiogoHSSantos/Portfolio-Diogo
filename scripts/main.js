938091181b998a9ac18122c82cb298f91f7ac1ae
DiogoHSSantos <115512182+DiogoHSSantos@users.noreply.github.com>
2023-03-11 20:52:16 -0300

    arrumei o site

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 const menuBurger = document.querySelector('.menuBurger');
 const menu = document.querySelector('.nav');
-const itemMenu = document.querySelectorAll('.itemMenu')
+const itemMenu = document.querySelectorAll('.itemMenu');
+const container = document.querySelectorAll('.container');
 
 menuBurger.addEventListener('click', () => {
     menu.classList.toggle('active')
@@ -9,6 +10,13 @@ menuBurger.addEventListener('click', () => {
 
 for(let i = 0 ; i < itemMenu.length ; i++){
     itemMenu[i].addEventListener('click', () => {
+        menu.classList.remove('active')
+        menuBurger.classList.remove('active')
+    })
+}
+
+for(let i = 0 ; i < container.length ; i++){
+    container[i].addEventListener('click', () => {
         menu.classList.remove('active')
         menuBurger.classList.remove('active')
     })
