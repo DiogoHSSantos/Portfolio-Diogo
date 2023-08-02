7521173a4e516c548f18269e7a745b67266798ff
DiogoHSSantos <115512182+DiogoHSSantos@users.noreply.github.com>
2023-08-02 09:08:21 -0300

    recriando o portfolio do zero

---
+++ portfolio-react/src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/main.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="header"></div>
+      <div id='main'></div>
+      <div id="footer"></div>
+    </>
+  )
+}
+
+export default App
